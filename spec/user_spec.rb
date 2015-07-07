8833c786c1dae217f97de3854007b22c26570a46
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-07-07 14:50:05 -0400

    refactor test to use before

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,21 +5,23 @@ describe User do
   describe "#new" do
     it "returns a user with a name" do
       user = User.new("raf","password")
+
       expect(user).to be_a(User)
       expect(user.name).to eq("raf")
     end
   end
 
   describe "#verify_password" do
+    before :each do
+      @user = User.new("raf","password")
+    end
+
     it "returns true for matching password" do
-      user = User.new("raf","password")
-      expect(user.verify_password("password")).to be_truthy
+      expect(@user.verify_password("password")).to be_truthy
     end
 
     it "returns false for wrong password" do
-      user = User.new("raf","password")
-      expect(user).to be_a(User)
-      expect(user.verify_password("wrong")).to be_falsey
+      expect(@user.verify_password("wrong")).to be_falsey
     end
   end
 end
