59a528b28eba41b351722fd2943b6f06fef5d0b0
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-07-07 14:26:43 -0400

    refactor w/ acccessors

--- lib/user.rb
+++ lib/user.rb
@@ -1,14 +1,8 @@
 class User
+  attr_accessor :name, :password
+
   def initialize(name, password)
     @name = name
     @password = password
-  end
-
-  def name
-    @name
-  end
-
-  def password
-    @password
   end
 end
