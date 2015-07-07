ccc30526945501b8c1883eb65044fe7bfc5eb949
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-07-07 14:47:47 -0400

    refactor tests to be more idiomatic

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,22 +1,25 @@
 require_relative 'spec_helper'
 require_relative '../lib/user.rb'
 
-RSpec.describe User, "#new" do
-  it "returns a user with a name" do
-    user = User.new("raf","password")
-    expect(user).to be_a(User)
-    expect(user.name).to eq("raf")
+describe User do
+  describe "#new" do
+    it "returns a user with a name" do
+      user = User.new("raf","password")
+      expect(user).to be_a(User)
+      expect(user.name).to eq("raf")
+    end
   end
-end
 
-RSpec.describe User, "#verify_password" do
-  it "returns true for matching password" do
-    user = User.new("raf","password")
-    expect(user.verify_password("password")).to be_truthy
-  end
-  it "returns false for wrong password" do
-    user = User.new("raf","password")
-    expect(user).to be_a(User)
-    expect(user.verify_password("wrong")).to be_falsey
+  describe "#verify_password" do
+    it "returns true for matching password" do
+      user = User.new("raf","password")
+      expect(user.verify_password("password")).to be_truthy
+    end
+
+    it "returns false for wrong password" do
+      user = User.new("raf","password")
+      expect(user).to be_a(User)
+      expect(user.verify_password("wrong")).to be_falsey
+    end
   end
 end
