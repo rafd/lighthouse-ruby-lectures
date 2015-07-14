2559b63033d0893d166e5bea88d28ddcc60d1dec
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-07-14 10:11:30 -0400

    working save

--- country.rb
+++ country.rb
@@ -1,7 +1,9 @@
 class Country
+  attr_accessor :name, :population, :capital, :area
+  attr_reader :id
 
   def initialize(hash)
-    @id = hash["id"].to_i
+    @id = hash["id"].to_i if hash["id"]
     @name = hash["name"]
     @population = hash["population"].to_i
     @capital = hash["capital"]
@@ -11,19 +13,27 @@ class Country
 
   def save
     # persist to db (insert or update)
-
+    if id
+      sql = 'UPDATE countries SET name=$1, population=$2, capital=$3, area=$4 WHERE id=$5;'
+      self.class.connection.exec_params(sql,[name, population,capital,area,id])
+    else
+      sql = 'INSERT INTO countries (name,population,capital,area) VALUES ($1,$2,$3,$4) RETURNING id'
+      result = self.class.connection.exec_params(sql,[name, population,capital,area])
+      id = result[0]["id"].to_i
+    end
   end
 
+  def self.connection
+    PG.connect(
+      host: 'localhost',
+      dbname: 'w3d2lecture'
+    )
+  end
 
   def self.all
     # query db for all 'countries' and return array of country objects
 
-    conn = PG.connect(
-      host: 'localhost',
-      dbname: 'w3d2lecture'
-    )
-
-    conn.exec('SELECT * FROM countries;') do |results|
+    self.connection.exec('SELECT * FROM countries;') do |results|
       results.map do |hash|
         Country.new(hash)
       end
