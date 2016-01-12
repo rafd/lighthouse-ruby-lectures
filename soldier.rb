73218237685a5912613ec2d5390c9c9002249459
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-01-12 11:40:54 -0500

    clean up soldier.rb example

--- soldier.rb
+++ soldier.rb
@@ -1,22 +1,46 @@
 # re: stubs/mocks
 
 class Soldier
-  attr_reader :health
+  attr_reader :health # this creates a health method
 
-  def inititialize(health)
-    @health = health
+  def initialize()
+    @health = 100
   end
 
   def can_attack?
-    # must use health and not @health, or else stub below will be useless
-    health > 10
+    # @health accesses the instance variable directly
+    @health > 10  # this can't be stubbed out
+    # health calls the instance method created by the attr_reader
+    health > 10  # this can be stubbed out
+  end
+
+  def attacked()
+    @health -= 1
   end
 end
 
 describe Soldier do
   describe "#can_attack" do
     it "can attack if health > 10" do
+      soldier = Soldier.new()
+
+      # could do this, but annoying:
+      # soldier.attacked()
+      # soldier.attacked()
+      # soldier.attacked()
+      # soldier.attacked()
+
+      # stubbing overwrites the attr_reader method:
+      # from:
+      # def health
+      #   @health
+      # end
+      # to:
+      # def health
+      #   5
+      # end
       soldier.stub(:health).and_return 5
+
       expects(soldier.can_attack?).to be(false)
     end
   end
